9703577f767d514383c261b380299783024d7f44
tox-in <habimanat78@gmail.com>
2026-02-12 01:04:00 +0200

    menu bar and navigation bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="w-full h-10 fixed top-8">
+      <h1 className="text-2xl font-bold text-center py-4">Json Placeholder</h1>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
